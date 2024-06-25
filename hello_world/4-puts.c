a780fe849a561a31ba274081e681ec55222c73e9
garryd13892 <garryd13892@github.com>
2024-06-25 08:31:59 +0000

    Put

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
 * main-start of program to put 
-* return: 0 for success 
+* return 0 at end 
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return(0);
 }
 
