95f9833b8f4b78d98916778f09ec9a1e326ab0e5
garryd13892 <garryd13892@github.com>
2024-06-25 16:13:48 +0000

    Printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Main: Printing using printf
- * Return: 0 sucess
- */
+ * main:Printing output using printf
+ * return:0 if sucess
+*/
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
