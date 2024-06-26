3bf1019530c22677217aa4c9b727cf8a74d31fda
garryd13892 <garryd13892@github.com>
2024-06-26 02:28:34 +0000

    Sizeof

---
+++ hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* Size of characters*/
+int main(void)
+{
+    printf("Size of char: %ld byte(s)\n", sizeof(char));
+    printf("Size of int: %ld byte(s)\n", sizeof(int));
+    printf("Size of long: %ld byte(s)\n", sizeof(long));
+    printf("Size of long long: %ld byte(s)\n", sizeof(long long));
+    printf("Size of float: %ld byte(s)\n", sizeof(float));
+    return(0);
+}
