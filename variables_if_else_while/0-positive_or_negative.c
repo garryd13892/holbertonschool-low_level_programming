c80b352a9d54a46dec23669aeb25434d87bbdb74
garryd13892 <garryd13892@github.com>
2024-06-26 10:24:51 +0000

    Posneg

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/* betty style doc for function main goes there */
+/**
+ *betty style doc for function main goes there
+ *main- a function to check is number is positive, zero or negative *
+ *return: Always 0 (success)
+*/
 int main(void)
 {
 	int n;
@@ -14,6 +17,6 @@ int main(void)
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
-	printf("%d is negative\n", n);	
+		printf("%d is negative\n", n);
 	return (0);
 }
