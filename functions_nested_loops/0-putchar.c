2c261fe8d25f3f0afeaf179c0154a753bf18e042
garryd13892 <garryd13892@github.com>
2024-07-01 02:39:21 +0000

    Put

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,7 @@
+#include "main.h"
 #include <stdio.h>
-
 int main (void)
 {
-	int _putchar(char c);
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -12,4 +10,7 @@ int main (void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
---
+++ functions_nested_loops/main.c
@@ -0,0 +1,7 @@
+#include <unistd.h>
+
+int _putchar(char c)
+{
+	printf("%c",c)
+	return (0);
+}
