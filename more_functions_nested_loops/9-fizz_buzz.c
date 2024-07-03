22c3db33073a4e09473c9f7c90f989fcf99dacae
garryd13892 <garryd13892@github.com>
2024-07-03 13:36:28 +0000

    Fizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int i=1;
+
+	while(i<=100)
+	{
+		if((i%3)==0 && (i%5)==0)
+                        printf("FizzBuzz ");
+		else if((i%3)==0)
+			printf("Fizz ");
+		else if((i%5)==0)
+			printf("Buzz ");
+		else
+			printf("%d ",i);
+		i++;
+	}
+	printf("\n");
+	return 0;
+}
