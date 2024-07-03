dcf92af85ea60caf73b9ccce37db32d62a1dfa35
garryd13892 <garryd13892@github.com>
2024-07-03 00:44:17 +0000

    to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -4,23 +4,29 @@ void print_to_98(int n)
 {
 	if (n>98)
 	{
-		printf("%d, ",n);
 		while(n>98)
 		{
-			n--;
 			printf("%d, ",n);
+			n--;
+			if(n==98)
+			{
+				printf("%d",n);
+			}
 		}
 	}
 	else if (n<98)
 	{
-		printf("%d, ",n);
 		while(n<98)
 		{
-			n++;
 			printf("%d, ",n);
+			n++;
+			if(n==98)
+			{
+				printf("%d",n);
+			}
 		}
 	}
-	else
+	else if(n==98)
 		printf("%d",n);
 	printf("\n");
 }
