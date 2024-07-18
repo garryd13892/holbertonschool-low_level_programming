54ae6b33aa14d7d97f1ab0c7c9909e7efc97699d
garryd13892 <garryd13892@github.com>
2024-07-18 08:33:05 +0000

    Revstring

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,15 +1,16 @@
 #include "main.h"
-#include <string.h>
+#include <stdio.h>
 
 void rev_string(char *s)
 {
-	int length = strlen(s);
-       	int i, j;
-       	char temp;
-       	for (i = 0, j = length - 1; i < j; i++, j--)
-       	{
-        temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-	}
+	char *end = s;
+	while (*end != '\0')
+    {
+        end++;
+    }
+    for (end--; end >= s; end--)
+    {
+        putchar(*end);
+    }
+    putchar('\n');
 }
