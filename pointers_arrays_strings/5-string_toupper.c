d06b471cfa734e21bc5fa32b391d8a7697803d70
garryd13892 <garryd13892@github.com>
2024-08-05 02:45:42 +0000

    to upper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+i#include "main.h"
+
+/**
+ * *string_toupper - check the code
+ *@c: pointer to array first element
+ *
+ * Return: Always 0.
+ */
+char *string_toupper(char *c)
+{
+	int count = 0;
+	int n;
+
+	while (c[count] != '\0')
+	{
+		n = c[count];
+
+		if (n >= 97 && n <= 122)
+		{
+			c[count] = n - 32;
+		}
+
+		count = count + 1;
+	}
+	return (c);
+}
